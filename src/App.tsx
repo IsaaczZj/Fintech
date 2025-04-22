4bc7492cceb422c9f7dfa71531ae850899130692
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-04-22 08:31:33 -0300

    criando paginas e hook useFetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import './Style.css'
+import Header from "./Components/Header";
+import SideNav from "./Components/SideNav";
+import Resumo from "./Pages/Resumo";
+import "./Style.css";
 function App() {
- 
-
   return (
-    <h1>
-      Fintech
-    </h1>
-  )
+    <div>
+      <SideNav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
