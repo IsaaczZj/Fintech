131522152b5623076802a1aa8c923f08426a0055
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-04-25 11:59:16 -0300

    Definindo as rotas da aplicacao

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import DateRange from "./DateRange";
 import Meses from "./Meses";
+import { useLocation } from "react-router";
 
 const Header = () => {
   const [title, setTitle] = useState("Resumo");
+  const location = useLocation();
 
+  useEffect(() => {
+    if (location.pathname === "/vendas") {
+      setTitle("Vendas");
+      document.title = "Fintech | Vendas";
+    } else if (location.pathname === "/") {
+      document.title = "Fintech | Resumo";
+
+      setTitle("Resumo");
+    }
+  }, [location]);
   return (
     <header className="mb">
       <div className="mb daterange">
--- src/Components/SideNav.tsx
+++ src/Components/SideNav.tsx
@@ -7,6 +7,7 @@ import configuracoes from "../assets/icons/configuracoes.svg";
 import contato from "../assets/icons/contato.svg";
 import sair from "../assets/icons/sair.svg";
 import FintechSVG from "../assets/FintechSVG";
+import { NavLink } from "react-router";
 
 const SideNav = () => {
   return (
@@ -17,13 +18,13 @@ const SideNav = () => {
           <span>
             <img src={resumo} alt="" />
           </span>
-          <a href="">Resumo</a>
+          <NavLink to="/">Resumo</NavLink>
         </li>
         <li>
           <span>
             <img src={vendas} alt="" />
           </span>
-          <a href="">Vendas</a>
+          <NavLink to="/vendas">Vendas</NavLink>
         </li>
         <li>
           <span>
